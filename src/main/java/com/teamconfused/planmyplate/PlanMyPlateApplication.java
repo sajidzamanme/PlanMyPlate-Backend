367fba255144b324a14b990904d52befb67733dc
Sajid Zaman <sajidzaman.2002.3@gmail.com>
2026-01-13 23:25:27 +0600

    Initial commit

---
+++ src/main/java/com/teamconfused/planmyplate/PlanMyPlateApplication.java
@@ -0,0 +1,13 @@
+package com.teamconfused.planmyplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanMyPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanMyPlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamconfused/planmyplate/PlanMyPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamconfused.planmyplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanMyPlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
